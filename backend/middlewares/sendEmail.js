c87ad2ae876e81ccb3fec706e4b8c30877f5c507
Swati More <swatimore7288@gmail.com>
2022-12-03 10:26:20 +0530

    initial commit

---
+++ backend/middlewares/sendEmail.js
@@ -0,0 +1,32 @@
+const nodeMailer = require("nodemailer");
+
+// exports.sendEmail = async (options) => {
+//   const transporter = nodeMailer.createTransport({
+//     host: "smtp.mailtrap.io",
+//     port: 2525,
+//     auth: {
+//       user: "211d4679e3d79c",
+//       pass: "9e2b7d43009e26",
+//     },
+//   });
+
+exports.sendEmail = async (options) => {
+  const transporter = nodeMailer.createTransport({
+    host: process.env.SMPT_HOST,
+    port: process.env.SMPT_PORT,
+    service: process.env.SMPT_SERVICE,
+    auth: {
+      user: process.env.SMPT_MAIL,
+      pass: process.env.SMPT_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.SMPT_MAIL,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
